26f40c7381692bbcee0617fa3f4f4609c1cbaf2a
Shigma <shigma10826@gmail.com>
2022-08-09 05:20:05 +0800

    feat: check outFile and rootDir

--- src/build.ts
+++ src/build.ts
@@ -1,12 +1,11 @@
-import fs from 'fs-extra'
-import tsconfig, { compile, option } from 'tsconfig-utils'
+import fs from 'fs/promises'
+import { compile, load, TsConfig } from 'tsconfig-utils'
 import { createRequire } from 'module'
 import { join, resolve } from 'path'
 import { bundle } from './bundle'
-import { SpawnOptions } from 'child_process'
 
 declare module 'tsconfig-utils' {
-  interface tsconfig {
+  interface TsConfig {
     dtsc: Config
   }
 }
@@ -15,16 +14,13 @@ export interface Config {
   inline?: string[]
 }
 
-async function compileToFile(filename: string, args: string[], options?: SpawnOptions) {
+async function compileToFile(filename: string, config: TsConfig) {
   filename = filename.replace(/\.d\.ts$/, '') + '.tmp.d.ts'
-  option(args, ['--composite'])
-  option(args, ['--incremental'])
-  await compile([
-    '--outFile', filename,
-    '--composite', 'false',
-    '--incremental', 'false',
-    ...args,
-  ], options)
+  config.set('project', '.', false)
+  config.set('outFile', filename)
+  config.set('composite', 'false')
+  config.set('incremental', 'false')
+  await compile(config.args, { cwd: config.cwd })
   const content = await fs.readFile(filename, 'utf8')
   await fs.rm(filename)
   return content
@@ -45,20 +41,17 @@ async function getModules(path: string, prefix = ''): Promise<string[]> {
 
 export async function build(cwd: string, args: string[] = []) {
   const require = createRequire(cwd + '/')
-  const filename = option(args, ['-p', '--project'], () => {
-    args.push('-p', '.')
-    return 'tsconfig.json'
-  }, true)
-
-  const config = await tsconfig(resolve(cwd, filename))
-  const { outFile, rootDir } = config.compilerOptions
-  if (!outFile) return compile(args)
+  const config = await load(cwd, args)
+  const outFile = config.get('outFile')
+  if (!outFile) throw new Error('outFile is required')
+  const rootDir = config.get('rootDir')
+  if (!rootDir) throw new Error('rootDir is required')
 
   const srcpath = `${cwd.replace(/\\/g, '/')}/${rootDir}`
-  const destpath = resolve(cwd, option(args, ['--outfile'], () => outFile))
+  const destpath = resolve(cwd, outFile)
   const [files, input] = await Promise.all([
     getModules(srcpath),
-    compileToFile(destpath, args, { cwd }),
+    compileToFile(destpath, config),
   ])
 
   let source = input
